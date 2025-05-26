05f875c326dc6ab8d32e52c2854a9ddecac26d08
luszczewskakasia <kasia.luszczewska13@gmail.com>
2025-05-26 20:22:33 +0200

    add docker export-csv container

--- export_to_csv/export_to_csv.py
+++ export_to_csv/export_to_csv.py
@@ -6,19 +6,22 @@ from database import SensorData, db
 import io, csv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@192.168.0.101:5432/postgres"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@sensor_db/postgres"
 db.init_app(app)
 CORS(app)
-
 
 @app.route('/api/data/export_to_csv', methods=['GET'])
 def export_to_csv():
     dest = io.StringIO()
     writer = csv.writer(dest)
-    sensor_data = SensorData.query.all()
+    writer.writerow(['Timestamp', 'Sensor Type', 'Value'])
+    limit = request.args.get('limit', default=20, type=int)
+    sensor_type = request.args.get('type', default='Temperature', type=str)
+    sensor_data = SensorData.query.filter_by(sensor_type=sensor_type).limit(limit).all()
     for row in sensor_data:
         writer.writerow([row.time, row.sensor_type, row.sensor_value])
     output = make_response(dest.getvalue())
-    # output.headers["Content-Disposition"] = "attachment; filename=sensor_data.csv"
-    output.headers["Content-type"] = "text/plain"
+    output.headers["Content-Disposition"] = f"attachment; filename=sensor_data_{sensor_type}_{limit}.csv"
+    output.headers["Content-type"] = "text/csv"
+    output.headers["Access-Control-Allow-Origin"] = "*"
     return output
