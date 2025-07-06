204df742f8ec556a929578681a6bcc0e67f3936b
luszczewskakasia <kasia.luszczewska13@gmail.com>
2025-07-06 21:14:18 +0200

    add file

--- time_metrics_collection.sh
+++ time_metrics_collection.sh
@@ -2,60 +2,79 @@
 
 OUTPUT_FILE="time.csv"
 NUM_ITERATIONS=10
-
-# Initialize CSV file with headers
+kubectl
 echo "iteration,start_time,deployment_time,stop_time,delete_time" > "$OUTPUT_FILE"
 
 measure_times() {
     local iteration=$1
-
-    # Measure minikube start time
-    echo "[INFO] Measuring minikube start time - iteration $iteration"
-    start_time=$(date +%s.%N)
     curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="server" sh -s - --node-name k3s-master
     sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-    sudo chown $USER:$USER ~/.kube/config
+    sudo chown $USER:$USER ~/.kube/config  
+    echo '
+    mirrors:
+    "192.168.100.15:8000":
+        endpoint:
+        - "http://192.168.100.15:8000"
+    ' | sudo tee /etc/rancher/k3s/registries.yaml
+    echo "start time - iteration $iteration"
+    start_time=$(date +%s.%N)
+    sudo systemctl start k3s
     end_time=$(date +%s.%N)
     start_duration=$(echo "$end_time - $start_time" | bc)
 
-    # Measure deployment time
     echo "[INFO] Measuring deployment time - iteration $iteration"
     start_time=$(date +%s.%N)
+    helm install sensor-db-postgresql bitnami/postgresql --set auth.postgresPassword=postgres --set volumePermissions.enabled=true
+    helm install rabbitmq bitnami/rabbitmq --set auth.username=guest --set auth.password=guest --set auth.forcePassword=true --set rabbitmq.extraConfiguration='loopback_users = none'
+    helm install ingress ingress-nginx/ingress-nginx --set controller.service.nodePorts.http=31145
     kubectl apply -f server/server-deployment.yml
     kubectl apply -f consumer/consumer-deployment.yml
     kubectl apply -f producer/producer-deployment.yml
     kubectl apply -f export_to_csv/export-csv-deployment.yml
     kubectl apply -f display_graph/display-graph-deployment.yml
     kubectl apply -f get_min_max_avg/get-values-deployment.yml
-    helm install sensor-db-postgresql bitnami/postgresql --set auth.postgresPassword=postgres --set volumePermissions.enabled=true
-    helm install rabbitmq bitnami/rabbitmq --set auth.username=guest --set auth.password=guest --set auth.forcePassword=true --set rabbitmq.extraConfiguration='loopback_users = none'
+    # kubectl wait --for=jsonpath='{.status.phase}'=Running pod -l app.kubernetes.io/instance=ingress
+    iter=1
+    until kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=ingress --timeout=5s; do
+        echo "[INFO] Waiting for ingress to be ready - iteration $iter"
+        sleep 5
+        ((iter++))
+    done
+    sleep 5
+
+    kubectl apply -f server/ingress.yml
+    kubectl apply -f export_to_csv/ingress.yml
+    kubectl apply -f display_graph/ingress.yml
+    kubectl apply -f get_min_max_avg/ingress.yml
+    until kubectl wait --for=condition=Ready pod --all --timeout=5s && kubectl wait --for=jsonpath='{.status.phase}'=Running pod --all --timeout=5s; do
+        echo "[INFO] Waiting for ingress to be ready - iteration $iter"
+        sleep 5
+        ((iter++))
+    done
+
     end_time=$(date +%s.%N)
+    kubectl get pods
     deploy_duration=$(echo "$end_time - $start_time" | bc)
 
-    # Measure minikube stop time
-    echo "[INFO] Measuring minikube stop time - iteration $iteration"
+    echo "[INFO] Measuring stop time - iteration $iteration"
     start_time=$(date +%s.%N)
     sudo systemctl stop k3s
     end_time=$(date +%s.%N)
     stop_duration=$(echo "$end_time - $start_time" | bc)
 
-    # Measure minikube delete time
-    echo "[INFO] Measuring minikube delete time - iteration $iteration"
+    echo "[INFO] Measuring delete time - iteration $iteration"
     start_time=$(date +%s.%N)
     sudo /usr/local/bin/k3s-uninstall.sh
     end_time=$(date +%s.%N)
     delete_duration=$(echo "$end_time - $start_time" | bc)
 
-    # Save results to CSV
     echo "$iteration,$start_duration,$deploy_duration,$stop_duration,$delete_duration" >> "$OUTPUT_FILE"
 }
 
-# Main loop
 for i in $(seq 1 $NUM_ITERATIONS); do
     echo "[INFO] Starting iteration $i of $NUM_ITERATIONS"
     measure_times $i
     echo "[INFO] Completed iteration $i"
-    echo "-------------------"
 done
 
 echo "[INFO] All measurements completed. Results saved in $OUTPUT_FILE"
